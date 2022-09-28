7ebe28d8a7cf555f1df3e89c9ff7cb6148e4dc9a
RebeccaWatley <89473987+RebeccaWatley@users.noreply.github.com>
2022-09-28 14:39:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello! Welcome to the PatientPortal</h1>
-    <h2>On this website you can find information about your Patient, their Doctor, and what day thier appointment is on!</h2>
+    <h2>Hello! Welcome to the PatientPortal</h2>
+    <h4>On this website you can find information about your Patient, their Doctor, and what day thier appointment is on!</h4>
   </body>
