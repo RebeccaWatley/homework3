023628987135b127b75726318a38eeb056061260
RebeccaWatley <89473987+RebeccaWatley@users.noreply.github.com>
2022-09-28 20:37:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
     <a class="btn btn-primary" href="Doctor.php" role="button">Doctor Information</a>
     <a class="btn btn-primary" href="Appointment.php" role="button">Appointment Information</a>
     <a class="btn btn-primary" href="Staff.php" role="button">Staff Information</a>
+    <a class="btn btn-primary" href="Patient-Cards.php" role="button">Show Patient Detials</a>
   
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
