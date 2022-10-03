4601e142fa4559d052777c9b40d7823d3548a312
RebeccaWatley <89473987+RebeccaWatley@users.noreply.github.com>
2022-10-03 15:26:07 -0500

    Update Patient.php

--- Patient.php
+++ Patient.php
@@ -58,12 +58,4 @@ $conn->close();
   </body>
 </html>
 
-
-
-    <form action="handlepost.php" method="get">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-    
    
